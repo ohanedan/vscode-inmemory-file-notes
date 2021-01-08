60abcdc83624f4f16406eefef5b8faa84db3324c
Ozan Hanedan <oznhndn@gmail.com>
2021-01-08 22:21:29 +0300

    update package.json

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Inmemory File Notes
 
 Creating inmemory file notes and save if needed
+
+## this extension is currently under development.
--- src/extension.ts
+++
@@ -1,7 +0,0 @@
-import * as vscode from 'vscode';
-
-export function activate(context: vscode.ExtensionContext) {
-	console.log('Congratulations, your extension "inmemory-file-notes" is now active!');
-}
-
-export function deactivate() { }
